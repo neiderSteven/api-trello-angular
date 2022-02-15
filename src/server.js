25f559480e397a6a008c6ee55984265974b515c9
neiderSteven <rodriguezneiderstiven@gmail.com>
2022-02-14 23:07:45 -0500

    configurando server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/trello'));
+app.use(express.static(__dirname + '/dist/trello'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/trello/index.html'));
+});
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/trello/'}),
-);
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
